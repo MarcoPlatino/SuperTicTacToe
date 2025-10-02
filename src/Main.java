098324263612a1bf92c5dfeafce53b79c06dcf16
Marco Platino <mistermarcop@hotmail.com>
2025-10-02 17:19:40 -0400

    Fixed messed up commit

--- src/Main.java
+++ src/Main.java
@@ -17,8 +17,24 @@ public class Main extends JFrame implements ActionListener {
 		}
 	}
 
-	public static void printOutBoardFormatted(char[][] board) {
-		System.out.println("Eventually the board will be printed out in a formatted way!");
+	// public static void printOutBoardFormatted(char[][] board) {
+	// 	System.out.println("Eventually the board will be printed out in a formatted way!");
+	// }
+
+	public Main() {
+		JFrame frame = new JFrame("RGBconverterGUI");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setPreferredSize(new Dimension(1000, 250));
+
+		JPanel contentpane = new JPanel();
+		contentpane.setLayout(new GridLayout(4, 4));
+
+		frame.setContentPane(contentpane);
+		
+		contentpane.setPreferredSize(new Dimension(750, 100));
+
+		frame.pack();
+		frame.setVisible(true);
 	}
 
 	public static void main(String[] args) {
@@ -40,5 +56,11 @@ public class Main extends JFrame implements ActionListener {
 				new Main();
 			}
 		});
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
 	}
 }
